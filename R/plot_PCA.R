8b0cc51468a054b938296dd80ba86f371676ebb5
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2021-10-07 14:06:11 +0200

    Added PCA plot and heatmaps

---
+++ R/plot_PCA.R
@@ -0,0 +1,61 @@
+#' Title
+#'
+#' @param ribo 
+#' @param col_to_plot 
+#' @param order_by_col 
+#' @param col_for_color 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_PCA <- function(ribo,col_to_plot,order_by_col,col_for_color = NULL) {
+  
+  pca_matrix <- aggregate_samples_by_col(ribo[["raw_counts"]],col_to_plot,position_to_rownames = T)
+  
+  pca_calculated <- .calculate_pca(pca_matrix, ribo[["metadata"]],order_by_col)
+  
+  return(.plot_pca(pca_calculated,ribo[["metadata"]],col_for_color))
+  
+}
+
+
+#' Title
+#'
+#' @param cscore.matrix 
+#' @param metadata 
+#' @param order.by.col 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+.calculate_pca <- function(cscore.matrix = NULL, metadata = NULL, order.by.col = NULL) {
+  pca.res <- dudi.pca(t(cscore.matrix[complete.cases(cscore.matrix), match(metadata[,order.by.col], colnames(cscore.matrix))]), 
+                      scannf = F, 
+                      nf = 5)
+  
+  return(pca.res)
+}
+
+
+#' Title
+#'
+#' @param dudi.pca 
+#' @param metadata 
+#' @param col.by.col 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+.plot_pca <- function(dudi.pca = NULL, metadata = NULL, col.by.col = NULL) {
+  plot.pca <- fviz_pca_ind(dudi.pca, 
+                           title = paste("PCA of Cscore for", as.character(ncol(dudi.pca$tab)), "sites."),
+                           repel = T, 
+                           habillage = metadata[,col.by.col],
+                           pointsize = 2, 
+                           labelsize = 4) + theme(text = element_text(size = 18)) + labs(subtitle = paste(length(metadata[,"filename"]),"samples"))
+  
+  return(plot.pca)
+}
---
+++ R/plot_heatmap.R
@@ -0,0 +1,139 @@
+#' plot heatmap for a ribo
+#'
+#' @param ribo 
+#' @param col_to_plot 
+#' @param order_by_col 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_heatmap <- function(ribo,col_to_plot,cols_for_annotation, order_by_col,most_variant=F) {
+  
+  matrix <- aggregate_samples_by_col(ribo[["raw_counts"]],col_to_plot,position_to_rownames = T)
+  .plot_heatmap(matrix,ribo[["metadata"]],order_by_col,cols_for_annotation = cols_for_annotation, most_variant = most_variant)
+  
+}
+
+#' Title
+#'
+#' @param cscore_matrix 
+#' @param annotation_samples 
+#' @param order_by_col 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+.plot_heatmap <- function(cscore_matrix = NULL, annotation_samples = NULL,cols_for_annotation = NULL, order_by_col = NULL, most_variant = F) {
+  
+  annotation_samples <- annotation_samples[,c(order_by_col, cols_for_annotation)]
+  # rownames of annotation_samples 
+  rownames(annotation_samples) <- annotation_samples[,order_by_col]
+  
+  
+  # all character columns to factor columns
+  annotation_samples[sapply(annotation_samples, is.character)] <- lapply(annotation_samples[sapply(annotation_samples, is.character)], 
+                                                                         as.factor)
+  
+  annotation_samples.1 <- annotation_samples[cols_for_annotation]
+  # library(RColorBrewer)
+  # list.cols <- list()
+  #  for (i in seq_along(colnames(annotation_samples.1))) {
+  #    
+  #  list.cols[[colnames(annotation_samples.1)[i]]] <- setNames(brewer.pal(nlevels(annotation_samples.1[, c(i)]), paste("Set", i, sep = "")), levels(annotation_samples.1[,c(i)]))
+  #    
+  #  }
+  
+  if(most_variant) {
+    cscore_matrix <- get_most_variant(cscore_matrix)
+  }
+  
+  htmap <- pheatmap::pheatmap(cscore_matrix[complete.cases(cscore_matrix),match(annotation_samples[,order_by_col], colnames(cscore_matrix))] ,
+                              clustering_method = "ward.D2" ,
+                              clustering_distance_cols = "manhattan", 
+                              clustering_distance_rows = "manhattan", 
+                              cutree_cols = 2, 
+                              cutree_rows = 4, 
+                              annotation_col = data.frame(annotation_samples.1),main = "heatmap")
+  return(htmap)
+  
+}
+
+# ==================================================
+
+#' plot a correlation heatmap from a riboclass object
+#'
+#' @param ribo 
+#' @param col_to_plot 
+#' @param cols_for_annotation 
+#' @param order_by_col 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_heatmap_corr <- function(ribo,col_to_plot,cols_for_annotation, order_by_col) {
+  
+  matrix <- aggregate_samples_by_col(ribo[["raw_counts"]],col_to_plot,position_to_rownames = T)
+  .plot_heatmap_corr(matrix,ribo[["metadata"]],order.by.col=order_by_col,cols_for_annotation = cols_for_annotation)
+  
+}
+#' Internal function of plot_heatmap_corr. 
+#'
+#' @param cscore.matrix 
+#' @param annotation.samples 
+#' @param cols_for_annotation 
+#' @param order.by.col 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+.plot_heatmap_corr <- function(cscore.matrix = NULL, annotation.samples = NULL,cols_for_annotation = NULL ,order.by.col = NULL) {
+  
+  # rownames of annotation.samples 
+  rownames(annotation.samples) <- annotation.samples[,order.by.col]
+  
+  
+  # all character columns to factor columns
+  annotation.samples[sapply(annotation.samples, is.character)] <- lapply(annotation.samples[sapply(annotation.samples, is.character)], 
+                                                                         as.factor)
+  corr_matrix <- 1- cor(cscore.matrix)
+  dist.cor <- as.dist(corr_matrix)
+  annotation.samples.1 <- annotation.samples[cols_for_annotation]
+  # library(RColorBrewer)
+  # list.cols <- list()
+  # for (i in seq_along(colnames(annotation.samples.1))) {
+  #   
+  #   list.cols[[colnames(annotation.samples.1)[i]]] <- setNames(brewer.pal(nlevels(annotation.samples.1[, c(i)]), paste("Set", i, sep = "")), levels(annotation.samples.1[,c(i)]))
+  #   
+  # }
+  
+  htmap <- pheatmap::pheatmap(corr_matrix[,match(annotation.samples[,order.by.col], colnames(corr_matrix))] ,
+                              clustering_method = "ward.D2" ,
+                              clustering_distance_cols = dist.cor, 
+                              clustering_distance_rows = dist.cor, 
+                              cutree_cols = 2, 
+                              cutree_rows = 4, 
+                              annotation_col = data.frame(annotation.samples.1),main = "correlation heatmap")
+  
+  return(htmap)
+  
+}
+
+get_most_variant <- function(df = NULL, column_or_row = "row", n = 20,i_want_most_var = TRUE) {
+  
+  df <- as.data.frame(df)
+  
+  if(column_or_row == "row") {column_or_row <- 1}
+  
+  if(column_or_row == "column") {column_or_row <- 2}
+  
+  if(is.null(df)) {stop("No matix in entry")}
+  
+  var <- apply(df, column_or_row, var)
+  
+  df[order(var, decreasing = i_want_most_var)[1:n],]
+  
+}
