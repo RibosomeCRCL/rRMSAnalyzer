4dd2e2b095927da8381e7f156f382aa5c08fb5b4
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2021-10-11 10:34:07 +0200

    New color scale + triangle mode + most variants selector

--- R/plot_heatmap.R
+++ R/plot_heatmap.R
@@ -3,6 +3,8 @@
 #' @param ribo 
 #' @param col_to_plot 
 #' @param order_by_col 
+#' @param cols_for_annotation 
+#' @param most_variant 
 #'
 #' @return
 #' @export
@@ -20,6 +22,8 @@ plot_heatmap <- function(ribo,col_to_plot,cols_for_annotation, order_by_col,most
 #' @param cscore_matrix 
 #' @param annotation_samples 
 #' @param order_by_col 
+#' @param cols_for_annotation 
+#' @param most_variant 
 #'
 #' @return
 #' @export
@@ -48,6 +52,7 @@ plot_heatmap <- function(ribo,col_to_plot,cols_for_annotation, order_by_col,most
   if(most_variant) {
     cscore_matrix <- get_most_variant(cscore_matrix)
   }
+
   
   htmap <- pheatmap::pheatmap(cscore_matrix[complete.cases(cscore_matrix),match(annotation_samples[,order_by_col], colnames(cscore_matrix))] ,
                               clustering_method = "ward.D2" ,
@@ -73,10 +78,10 @@ plot_heatmap <- function(ribo,col_to_plot,cols_for_annotation, order_by_col,most
 #' @export
 #'
 #' @examples
-plot_heatmap_corr <- function(ribo,col_to_plot,cols_for_annotation, order_by_col) {
+plot_heatmap_corr <- function(ribo,col_to_plot,cols_for_annotation, order_by_col,use_triangle=F) {
   
   matrix <- aggregate_samples_by_col(ribo[["raw_counts"]],col_to_plot,position_to_rownames = T)
-  .plot_heatmap_corr(matrix,ribo[["metadata"]],order.by.col=order_by_col,cols_for_annotation = cols_for_annotation)
+  .plot_heatmap_corr(matrix,ribo[["metadata"]],order.by.col=order_by_col,cols_for_annotation = cols_for_annotation,use_triangle)
   
 }
 #' Internal function of plot_heatmap_corr. 
@@ -85,12 +90,13 @@ plot_heatmap_corr <- function(ribo,col_to_plot,cols_for_annotation, order_by_col
 #' @param annotation.samples 
 #' @param cols_for_annotation 
 #' @param order.by.col 
+#' @param use_triangle 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-.plot_heatmap_corr <- function(cscore.matrix = NULL, annotation.samples = NULL,cols_for_annotation = NULL ,order.by.col = NULL) {
+.plot_heatmap_corr <- function(cscore.matrix = NULL, annotation.samples = NULL,cols_for_annotation = NULL ,order.by.col = NULL,use_triangle=F) {
   
   # rownames of annotation.samples 
   rownames(annotation.samples) <- annotation.samples[,order.by.col]
@@ -109,15 +115,30 @@ plot_heatmap_corr <- function(ribo,col_to_plot,cols_for_annotation, order_by_col
   #   list.cols[[colnames(annotation.samples.1)[i]]] <- setNames(brewer.pal(nlevels(annotation.samples.1[, c(i)]), paste("Set", i, sep = "")), levels(annotation.samples.1[,c(i)]))
   #   
   # }
+  white_red<-colorRampPalette(c("white","red"),interpolate = "linear")(100)
+  
+  if(use_triangle) {
+  corr_matrix[lower.tri(corr_matrix)] <- NA
   
   htmap <- pheatmap::pheatmap(corr_matrix[,match(annotation.samples[,order.by.col], colnames(corr_matrix))] ,
-                              clustering_method = "ward.D2" ,
-                              clustering_distance_cols = dist.cor, 
-                              clustering_distance_rows = dist.cor, 
-                              cutree_cols = 2, 
-                              cutree_rows = 4, 
+                              cluster_cols = F,
+                              cluster_rows = F,
+                              color = white_red,
+                              breaks = seq(0, 1, by = 0.01),
                               annotation_col = data.frame(annotation.samples.1),main = "correlation heatmap")
-  
+  }
+  else  {
+    htmap <- pheatmap::pheatmap(corr_matrix[,match(annotation.samples[,order.by.col], colnames(corr_matrix))] ,
+                                clustering_method = "ward.D2" ,
+                                clustering_distance_cols = dist.cor, 
+                                clustering_distance_rows = dist.cor, 
+                                cutree_cols = 2, 
+                                cutree_rows = 4, 
+                                color = white_red,
+                                breaks = seq(0, 1, by = 0.01),
+                                annotation_col = data.frame(annotation.samples.1),main = "correlation heatmap")
+    
+  }
   return(htmap)
   
 }
