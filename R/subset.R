4e1cff556fc961441c6ab4b8a334567594dcded2
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2021-10-21 16:10:16 +0200

    subset now throws a warning if unknown positions are given

--- R/subset.R
+++ R/subset.R
@@ -12,6 +12,16 @@ subset_ribo <- function(ribo, positions_to_keep) {
   
   ribo_data <- ribo[["raw_counts"]]
   
+  #check if positions_to_keep contains only existing positions. Throw a warning if not.
+  
+  existing_positions <- ribo[[1]][[1]][["named_position"]]
+  if (!isTRUE(positions_to_keep %in% existing_positions)) {
+    warning(paste(
+      "One or more of the given positions to keep do not exist :",
+      paste(positions_to_keep[!(positions_to_keep %in% existing_positions)], collapse = "; ")
+    ))
+  }
+    
   #iterates through samples and keep only specified positions
   
   subsetted_ribo_data <- lapply(ribo_data, function(x) {
