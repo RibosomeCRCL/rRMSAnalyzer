0423396a2c9814ca10fd4e358100d347716d05ea
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2021-10-21 14:25:16 +0200

    Better labels for plot_RLE

--- R/plot_RLE.R
+++ R/plot_RLE.R
@@ -13,7 +13,6 @@
   mat <- mat + 1
   med <- apply(mat, 1, median)
   rle <- log2(mat/med)
-  library(tidyr)
   rle <- gather(as.data.frame(rle))
   return(rle)
 }
@@ -29,14 +28,14 @@
 #' @export
 #'
 #' @examples
-.plot_RLE <- function(mat = NULL,show_outlier, ...) {
+.plot_RLE <- function(mat = NULL,show_outlier,col_to_plot) {
   stopifnot(!is.null(mat))
   mat <- as.data.frame(mat)
   rle.calc <- .calculate_relative_log_expression(mat = mat)
   outlier_shape=NA
   if(show_outlier) outlier_shape = 19
   
-  rle.plot <- ggplot(rle.calc, aes(x = key, value)) + geom_boxplot(outlier.shape =outlier_shape ,outlier.size = 0.3, fill = "red") + ylab("log2(C-score/median)") + xlab("Sample ID") + ggtitle("RLE plot") + theme(axis.text.x = element_text(angle=45, vjust = 0.8)) +  geom_hline(yintercept = 2*mad(rle.calc$value, na.rm = T), colour = "blue") + geom_hline(yintercept = - 2*mad(rle.calc$value, na.rm = T), colour = "blue")
+  rle.plot <- ggplot(rle.calc, aes(x = key, value)) + geom_boxplot(outlier.shape =outlier_shape ,outlier.size = 0.3, fill = "red") + ylab(paste0("log2(",col_to_plot,"/median)")) + xlab("Sample ID") + ggtitle("RLE plot") + theme(axis.text.x = element_text(angle=90 )) +  geom_hline(yintercept = 2*mad(rle.calc$value, na.rm = T), colour = "blue") + geom_hline(yintercept = - 2*mad(rle.calc$value, na.rm = T), colour = "blue")
   return(rle.plot)
 }
 
@@ -53,5 +52,5 @@
 plot_RLE <- function(ribo, col_to_plot,show_outlier) {
   
   rle_matrix <- aggregate_samples_by_col(ribo[["raw_counts"]],col_to_keep = col_to_plot,position_to_rownames = T)
-  return(.plot_RLE(rle_matrix,show_outlier = show_outlier))
+  return(.plot_RLE(rle_matrix,show_outlier = show_outlier,col_to_plot))
 }
