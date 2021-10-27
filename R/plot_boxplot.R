342902546a6e636a43bca6ef29f5348bb4048eca
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2021-10-27 15:22:52 +0200

    Plot boxplots by sites or by samples

--- R/plot_boxplot.R
+++ R/plot_boxplot.R
@@ -9,9 +9,35 @@
 #' @examples plot_boxplot_count(ribo,"group")
 plot_boxplot_count <- function(ribo, metadata_col=NA,show_outlier = T) {
   ribo_matrix <- aggregate_samples_by_col(ribo[[1]],"Count",position_to_rownames = T)
-  return(.plot_boxplot(ribo_matrix,"count","log10(count)",ribo[["metadata"]],metadata_col,show_outlier))
+  return(.plot_boxplot_samples(ribo_matrix,"count","log10(count)",ribo[["metadata"]],metadata_col,show_outlier))
 
   
+}
+
+#' Title
+#'
+#' @param matrix 
+#' @param values_col_name 
+#' @param values_to_plot 
+#' @param metadata 
+#' @param metadata_col 
+#' @param show_outlier 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+.plot_boxplot_sites <- function(matrix,values_col_name,values_to_plot, metadata,order_by,show_outlier, use_renamed_positions = T) {
+  id_vars <- "named_position"
+  
+  matrix_melted <- reshape2::melt(matrix, id.vars = id_vars,value.name = values_col_name,)
+  
+  shape_outlier <- NA
+  if(show_outlier) shape_outlier <- 19
+  print(matrix_melted)
+  p <- ggplot2::ggplot( matrix_melted, aes(x = reorder(named_position,!!sym(values_to_plot),na.rm = T), y = !!sym(values_to_plot))) +
+    geom_boxplot(outlier.shape = shape_outlier) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+  return(p)
 }
 
 #' (internal) plot boxplot for a given matrix of values
@@ -27,16 +53,30 @@ plot_boxplot_count <- function(ribo, metadata_col=NA,show_outlier = T) {
 #' @export
 #'
 #' @examples
-.plot_boxplot <- function(matrix,values_col_name,values_to_plot, metadata,metadata_col,show_outlier) {
-  matrix_inv <- t(matrix)
-  matrix_inv = cbind(matrix_inv,metadata[metadata_col]) #TODO : proper merge
+.plot_boxplot_samples <- function(matrix,values_col_name,values_to_plot, metadata,metadata_col=NA,show_outlier) {
+  id_vars <- "sample"
+  matrix_inv <- as.data.frame(t(matrix))
   matrix_inv <- tibble::rownames_to_column(matrix_inv,"sample")
-  matrix_melted <- reshape2::melt(matrix_inv, id.vars = c(metadata_col,"sample"),value.name = values_col_name)
+  
+  if(!is.na(metadata_col)){
+    matrix_inv <- cbind(matrix_inv,metadata[metadata_col])
+    id_vars <- c(metadata_col,"sample")
+    } #TODO : proper merge
+  
+  matrix_melted <- reshape2::melt(matrix_inv, id.vars = id_vars,value.name = values_col_name)
+  
 
   shape_outlier <- NA
   if(show_outlier) shape_outlier <- 19
   
+  if(is.na(metadata_col)) {
+    p <- ggplot2::ggplot(matrix_melted, aes_string(x = "sample", y = values_to_plot)) +
+      geom_boxplot(outlier.shape = shape_outlier) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+    return(p)
+  }
+  else {
   p <- ggplot2::ggplot(matrix_melted, aes_string(x = "sample", y = values_to_plot,fill = metadata_col)) +
     geom_boxplot(outlier.shape = shape_outlier) + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
   return(p)
+  }
 }
