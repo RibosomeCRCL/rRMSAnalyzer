a0c522488664cc211b05fe868a86557b7dccc707
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2022-01-13 14:46:31 +0100

    Rename positions according to renamed_position col

--- R/subset.R
+++ R/subset.R
@@ -1,21 +1,26 @@
 
-#' Title
+#' Keep only a subset of positions for all samples.
 #'
-#' @param ribo 
-#' @param positions_to_keep 
+#' @param ribo a riboClass object
+#' @param positions_to_keep either a dataframe with a column "named_position" or a vector of named positions.
 #'
-#' @return
+#' @return a riboClass object with only selected positions for each sample.
 #' @export
 #'
-#' @examples
+#' @examples subset_ribo(ribo, c("18S_15","5.8S_4")) # returns a riboClass with only 2 selected positions
 subset_ribo <- function(ribo, positions_to_keep) {
   
   ribo_data <- ribo[["counts"]]
   
-  #check if positions_to_keep contains only existing positions. Throw a warning if not.
+  if(is.data.frame(positions_to_keep)) {
+    positions_df <- positions_to_keep
+    positions_to_keep <- positions_to_keep[["named_position"]]
+    
+  }
   
+  #check if positions_to_keep contains only existing positions. Throw a warning if not.
   existing_positions <- ribo[[1]][[1]][["named_position"]]
-  if (!isTRUE(positions_to_keep %in% existing_positions)) {
+  if (!all(positions_to_keep %in% existing_positions)) {
     warning(paste(
       "One or more of the given positions to keep do not exist :",
       paste(positions_to_keep[!(positions_to_keep %in% existing_positions)], collapse = "; ")
@@ -26,6 +31,14 @@ subset_ribo <- function(ribo, positions_to_keep) {
   
   subsetted_ribo_data <- lapply(ribo_data, function(x) {
     x <- x[which(x[,"named_position"] %in% positions_to_keep),]
+    
+    # if a column "renamed_positions" exist, add it to the sample table. It will be used instead named_position.
+    if("renamed_position" %in% colnames(positions_df)) {
+    x["renamed_position"] <- positions_df[["renamed_position"]][match(x[["named_position"]],positions_df[["named_position"]])]
+    x["named_position"] <- x["renamed_position"]
+    x <- subset(x, select=-c(renamed_position))
+    }
+    
     return(x)
   })
   
