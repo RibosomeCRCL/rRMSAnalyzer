c80851c63875926f7b6dbbc7eca20ee7013b6634
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2022-01-25 14:40:22 +0100

    Conditionnal coloring for RLE

--- R/plot_RLE.R
+++ R/plot_RLE.R
@@ -34,9 +34,19 @@
   rle.calc <- .calculate_relative_log_expression(mat = mat)
   outlier_shape=NA
   if(show_outlier) outlier_shape = 19
+  mad <- - 2*mad(rle.calc$value, na.rm = T)
+  rle_grouped <- rle.calc %>% group_by(key) %>% summarise(median = median(value, na.rm = TRUE))
   
-  rle.plot <- ggplot(rle.calc, aes(x = key, value)) + geom_boxplot(outlier.shape =outlier_shape ,outlier.size = 0.3, fill = "red") + ylab(paste0("log2(",col_to_plot,"/median)")) + xlab("Sample ID") + ggtitle("RLE plot") + theme(axis.text.x = element_text(angle=90 )) +  geom_hline(yintercept = 2*mad(rle.calc$value, na.rm = T), colour = "blue") + geom_hline(yintercept = - 2*mad(rle.calc$value, na.rm = T), colour = "blue")
-  return(rle.plot)
+  rle.plot <- ggplot(rle.calc, aes(x = key, value)) + 
+    geom_boxplot(outlier.shape =outlier_shape ,outlier.size = 0.3, fill = ifelse(rle_grouped$median < mad, "red","white")) +
+    ylab(paste0("log2(",col_to_plot,"/median)")) +
+    xlab("Sample") +
+    ggtitle("RLE plot") + 
+    geom_hline(yintercept = 2*mad(rle.calc$value, na.rm = T), colour = "blue") + 
+    geom_hline(yintercept = - 2*mad(rle.calc$value, na.rm = T), colour = "blue") +
+    theme(axis.text.x = element_text(colour = ifelse(rle_grouped$median < mad,"red","black")))
+ 
+   return(rle.plot)
 }
 
 #' Title
