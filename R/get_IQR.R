796bcdd298bb7875567cec2191754a7aa66287d7
hermespara <hermes.paraqindes@etu.univ-lyon1.fr>
2022-02-14 16:37:37 +0100

    add new file with IQR code

---
+++ R/get_IQR.R
@@ -0,0 +1,73 @@
+get_IQR <- function(df.meth.scores = NULL, order = "IQR"){
+  
+  
+  #the df should have in rows 2'OMe sites and in columns the samples
+  df.meth.scores <- as.data.frame(df.meth.scores)
+  
+
+  if (tolower(order) == "iqr") {
+    df.meth.scores$iqr <- apply(df.meth.scores, 1, IQR) # compute the IQR
+    df.meth.scores$sites.id <- rownames(df.meth.scores)
+    
+    df.meth.scores <- tidyr::gather(df.meth.scores, "sampleID", "Cscore", -sites.id, -iqr) # Gather for site id and IQR
+    
+    df.meth.scores <- df.meth.scores[order(df.meth.scores$iqr),] # order by IQR
+    
+    df.meth.scores$sites.id <- factor(df.meth.scores$sites.id, levels = unique(df.meth.scores$sites.id)) # refactor the site.id
+  }
+  
+  if (tolower(order) == "var") {
+    
+    df.meth.scores$var <- apply(df.meth.scores, 1, var) # compute the var
+    df.meth.scores$sites.id <- rownames(df.meth.scores)
+    
+    df.meth.scores <- tidyr::gather(df.meth.scores, "sampleID", "Cscore", -sites.id, -var) # Gather for site id and var
+    
+    df.meth.scores <- df.meth.scores[order(df.meth.scores$var),] # order by var
+    
+    df.meth.scores$sites.id <- factor(df.meth.scores$sites.id, levels = unique(df.meth.scores$sites.id)) # refactor the site.id
+
+  }
+  
+  return(df.meth.scores)
+  
+}
+
+
+
+plot_sites_by_IQR <- function(ribo = NULL, plot = "IQR", variance = "IQR") {
+  
+  Cscore_matrix <- aggregate_samples_by_col(ribo[["counts"]],"cscore_median",position_to_rownames = T)
+  IQR_order_df <- get_IQR(Cscore_matrix, order = variance)
+  
+  
+  if (tolower(plot) == "iqr") {
+  
+  p1 <- ggplot(IQR_order_df, aes(x = sites.id, y = iqr)) +
+    geom_point() +
+    theme_bw() + 
+    theme(axis.text.x = element_text(angle = 45, size = 9, hjust = 1), 
+          legend.position = "top", 
+          axis.title=element_text(size=14), 
+          axis.text.y = element_text(size=12)) + 
+    labs(x = "rRNA 2'Ome sites",
+         y = "Interquartile Range (IQR)", size = 20)
+    
+  }
+  
+  if (tolower(plot) == "boxplot") {
+    
+    p1 <- ggplot(IQR_order_df, aes(x = sites.id, y = Cscore)) +
+      geom_boxplot() +
+      theme_bw() + 
+      theme(axis.text.x = element_text(angle = 45, size = 9, hjust = 1), 
+            legend.position = "top", 
+            axis.title=element_text(size=14), 
+            axis.text.y = element_text(size=12)) + 
+      labs(x = "rRNA 2'Ome sites",
+           y = "Cscore", size = 20)
+    
+  }
+  
+  return(p1)
+}
--- update.R
+++ update.R
@@ -1,33 +1,51 @@
 library(devtools)
 library(roxygen2)
 
-load(file = "/home/hermes/Desktop/RMS/R/sysdata.rda")
-load(file = "/home/hermes/Desktop/RMS/data/mouse.methylated.rda")
-load(file = "/home/hermes/Desktop/RMS/data/mouse.suspected.rda")
+# load(file = "/home/hermes/Desktop/RMS/R/sysdata.rda")
+# load(file = "/home/hermes/Desktop/RMS/data/mouse.methylated.rda")
+# load(file = "/home/hermes/Desktop/RMS/data/mouse.suspected.rda")
+# 
+# #mouse.methylated <- read.csv("/home/hermes/Desktop/home/hermes/Bureau/CDD/Mouse_vs_Human_vs/Last_time/known_sites_mouse.csv")
+# #mouse.suspected <- read.csv("/home/hermes/Desktop/home/hermes/Bureau/CDD/Mouse_vs_Human_vs/Last_time/suspected_sites_mouse.csv")
+# names(mouse.methylated)[4] <- "NR_046235.Numbering"
+# mouse.methylated$SNORD <- NA
+# mouse.methylated$Mode.of.coding <- NA
+# mouse.methylated$SNORD.host.gene <- NA
+# #mouse.methylated <- known
+# #mouse.suspected <- suspcted
+# 
+# all_z <- read.csv("/home/hermes/Desktop/home/hermes/Bureau/CDD/Mouse_vs_Human_vs/Other_Modification/all_zero.csv")
+# all_z$Nomenclature <- paste(all_z$Sequence, all_z$NR_003279.1_28SrRNA_mouse_position, sep = "")
+# 
+# mouse.suspected <- dplyr::anti_join(mouse.suspected, all_z, by = "Nomenclature")
+# use_data(mouse.suspected)
+# 
+# 
+# # change name of human
+# 
+# names(human.methylated)[4] <- "NR_046235.Numbering"
+# names(human.suspected)[4] <- "NR_046235.Numbering"
+# 
+# 
+# use_data(wdw.probas.5s, wdw.probas.18s, wdw.probas.28s, wdw.probas.5.8s, human.suspected, human.methylated, 
+#          mouse.wdw.probas.5.8s, mouse.wdw.probas.18s, mouse.wdw.probas.28s, mouse.suspected, mouse.methylated, internal=T, overwrite=T)
+# 
+# save(mouse.suspected, file = "/home/hermes/Desktop/RMS/data/mouse.suspected.rda")
 
-#mouse.methylated <- read.csv("/home/hermes/Desktop/home/hermes/Bureau/CDD/Mouse_vs_Human_vs/Last_time/known_sites_mouse.csv")
-#mouse.suspected <- read.csv("/home/hermes/Desktop/home/hermes/Bureau/CDD/Mouse_vs_Human_vs/Last_time/suspected_sites_mouse.csv")
-names(mouse.methylated)[4] <- "NR_046235.Numbering"
-mouse.methylated$SNORD <- NA
-mouse.methylated$Mode.of.coding <- NA
-mouse.methylated$SNORD.host.gene <- NA
-#mouse.methylated <- known
-#mouse.suspected <- suspcted
-
-all_z <- read.csv("/home/hermes/Desktop/home/hermes/Bureau/CDD/Mouse_vs_Human_vs/Other_Modification/all_zero.csv")
-all_z$Nomenclature <- paste(all_z$Sequence, all_z$NR_003279.1_28SrRNA_mouse_position, sep = "")
-
-mouse.suspected <- dplyr::anti_join(mouse.suspected, all_z, by = "Nomenclature")
-use_data(mouse.suspected)
 
 
-# change name of human
-
-names(human.methylated)[4] <- "NR_046235.Numbering"
-names(human.suspected)[4] <- "NR_046235.Numbering"
+# Update Human methylated and suspected sites
 
 
-use_data(wdw.probas.5s, wdw.probas.18s, wdw.probas.28s, wdw.probas.5.8s, human.suspected, human.methylated, 
-         mouse.wdw.probas.5.8s, mouse.wdw.probas.18s, mouse.wdw.probas.28s, mouse.suspected, mouse.methylated, internal=T, overwrite=T)
+load("data/human_methylated_28S.Rdata")
+load("data/human_methylated_18S.Rdata")
+load("data/human_methylated_58S.Rdata")
+load("data/human_suspected_18S.Rdata")
+load("data/human_suspected_28S.Rdata")
+load("data/human_suspected_58S.Rdata")
 
-save(mouse.suspected, file = "/home/hermes/Desktop/RMS/data/mouse.suspected.rda")
+human_methylated <- rbind(df.methylated.58S, rbind(df.methylated.18S, df.methylated.28S))
+colnames(df.suspected.18S)[9] <- "Ensembl"
+human_suspected <- rbind(df.suspected.5.8S, rbind(df.suspected.18S, df.suspected.28S))
+
+use_data(human_methylated, human_suspected, internal=F, overwrite=T)
