37963d3830857c085f6abcad0d4e286501d92e4d
hermespara <hermes.paraqindes@etu.univ-lyon1.fr>
2022-02-22 15:31:55 +0100

    adding rownames to metadata

--- R/load_input_data.R
+++ R/load_input_data.R
@@ -41,13 +41,17 @@ read_counts <- function(counts_path,
   else {
     if(is.character(metadata)) {
     metadata <- read.csv(metadata, sep = metadata_sep)
+    rownames(metadata) <- metadata[,"samplename"]
     }
     names(metadata)[names(metadata) == metadata_sample_name_col] <- "samplename"
+    
+    rownames(metadata) <- metadata[,"samplename"]
     
     # read count data
     rna_counts_dt <- read_count_files(counts_path,count_sep,counts_header,
                                       counts_rna_col,counts_rnapos_col,counts_count_col,
                                       files_to_keep = as.character(metadata[,metadata_filename_col]))
+    # generate RNA names table
     rna_names_df <- generate_rna_names_table(rna_counts_dt[[1]])
     
     # Rename sample in counts list according to the names in metadata
