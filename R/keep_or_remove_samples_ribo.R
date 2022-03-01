9213d46918b9631b1201cd619ff4315d96216ddd
hermespara <hermes.paraqindes@etu.univ-lyon1.fr>
2022-03-01 15:54:44 +0100

    add keep or delete samples function

---
+++ R/keep_or_remove_samples_ribo.R
@@ -0,0 +1,48 @@
+#' Keep only a selected number of samples
+#'
+#' @param ribo 
+#' @param samples_to_keep A vector with the samples to keep
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' ribo.test <- keep_ribo_samples(ribo = ribo, samples_to_keep = c("SAMPLE1", "SAMPLE2"))
+
+keep_ribo_samples <- function(ribo = NULL, samples_to_keep = NULL) {
+  
+  if(all(samples_to_keep %in% names(ribo[["counts"]]))) {
+    
+    ribo[["counts"]] <- ribo[["counts"]][samples_to_keep]
+    ribo[["metadata"]] <- ribo[["metadata"]][match(samples_to_keep, rownames(ribo[["metadata"]])),]
+    
+  } else {
+    stop(paste("Samples name should be from", toString(names(ribo[["counts"]]))))
+  }
+  return(ribo)
+}
+
+
+#' Delete samples from Riboclass object
+#'
+#' @param ribo 
+#' @param samples_to_delete A vector with the samples to keep
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' ribo.test <- delete_ribo_samples(ribo = ribo, samples_to_keep = c("SAMPLE1", "SAMPLE2"))
+
+delete_ribo_samples <- function(ribo = NULL, samples_to_delete = NULL) {
+  
+  if(all(samples_to_delete %in% names(ribo[["counts"]]))) {
+    idx <- match(samples_to_delete, names(ribo[["counts"]]))
+    ribo[["counts"]] <- ribo[["counts"]][-idx]
+    ribo[["metadata"]] <- ribo[["metadata"]][-match(samples_to_delete, rownames(ribo[["metadata"]])),]
+    
+  } else {
+    stop(paste("Samples name should be from", toString(names(ribo[["counts"]]))))
+  }
+  return(ribo)
+}
