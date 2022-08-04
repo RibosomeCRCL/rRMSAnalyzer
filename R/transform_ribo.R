4dbfd0473b3091794e62b01ff5fce9a5e9a7b369
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2022-08-04 11:17:11 +0200

    Change function and parameters' name

--- R/transform_ribo.R
+++ R/transform_ribo.R
@@ -5,40 +5,37 @@
 #'
 #' @return
 #' @export
-#'
-#' @examples
-#' transform.ribo(ribo = ribo, metadata_columns = c(5,6))
-#' transform.ribo(ribo = ribo.meth, metadata_columns = c("SAMPLE..ORIGIN", "SPECIES"))
+#' 
 
 
 
 
-transform_ribo <- function(ribo, metadata_columns = NULL) {
-  
+format_to_plot <- function(ribo, metadata_col = NULL) {
+  site <- NULL #NSE fix
   # First let's extract the data from ribo
   # TODO aggregate_samples_by_col function should be simplified, 
   # specially the values_column parameter which should be only "Cscore" or "Counts"
-  values_column <- "cscore_median"
-  df.matrix <- aggregate_samples_by_col(ribo[["counts"]],values_column,position_to_rownames = T)
+  values_column <- "cscore"
+  df.matrix <- extract_data(ribo,values_column,position_to_rownames = T)
   # Add a new columns that correspond to rownames
   # TODO make sure that aggregate_samples_by_col returns the good rownames (with the site Name)
   df.matrix$siteID <- rownames(df.matrix)
   # Transform the data with tidyr
-  df.tranform <- tidyr::gather(df.matrix, "sampleID", "Cscore", -siteID)
+  df.tranform <- tidyr::gather(df.matrix, "sampleID", "Cscore", -site)
   
-  if (is.null(metadata_columns)) {
+  if (is.null(metadata_col)) {
     # if no metadata, return the transformed data frame
     return(df.tranform)
   }
   else {
     # Check if the metadata_columns are numeric. 
     # If yes than get the column names of the selected columns
-    if (is.numeric(metadata_columns)){
-      metadata_columns <- colnames(ribo[["metadata"]][metadata_columns])
+    if (is.numeric(metadata_col)){
+      metadata_columns <- colnames(ribo[["metadata"]][metadata_col])
     }
     # Get the metadata
     # TODO samplename column should always exists in metadata
-    df.meta <- ribo.meth[["metadata"]][c("samplename",metadata_columns)]
+    df.meta <- ribo[["metadata"]][c("samplename",metadata_col)]
     # if metadata is empty, return an error
     if (nrow(df.meta) == 0) { stop("No metadata found") }
     # Merge the metadata with the transformed data frame
