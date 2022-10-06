4bbbbcd95323945414028975e83ce499d46204f5
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2022-10-06 16:10:48 +0200

    extract_data on annotated sites

--- R/extract_data.R
+++ R/extract_data.R
@@ -30,19 +30,38 @@ extract_data <- function(ribo, col = "cscore", position_to_rownames =F, only_ann
     stop(paste(col, "is not a column in the data !"))
   }
   
-  
   sample_list <- ribo[["data"]]
   sample_list_nm <- names(sample_list)
+  if(only_annotated) {
+    df <- sample_list[[3]]
+    df_sites <- df[which(!is.na(df[,"site"])),]
+    position_list <- df_sites[,"site"]
+    matrix_all <- data.frame(site = position_list)
+    
+  }
+  else {
   position_list <- sample_list[[3]][,"named_position"]
   matrix_all <- data.frame(named_position = position_list)
+  }
   
   for(sample_nm in sample_list_nm) {
     sample_df <- sample_list[[sample_nm]]
+    if(only_annotated) {
+      sample_df <- sample_df[which(!is.na(sample_df[,"site"])),]
+      sample_df <- sample_df[,c("site",col)]
+      matrix_all <- dplyr::full_join(matrix_all,sample_df,by="site")
+      names(matrix_all)[length(names(matrix_all))] <- sample_nm 
+      matrix_all <- matrix_all[match(position_list,matrix_all[,"site"]),]
+    }
+    else {
     sample_df <- sample_df[,c("named_position",col)]
     matrix_all <- dplyr::full_join(matrix_all,sample_df,by="named_position")
     names(matrix_all)[length(names(matrix_all))] <- sample_nm 
+    matrix_all <- matrix_all[match(position_list,matrix_all[,"named_position"]),]
+    
   }
-  matrix_all <- matrix_all[match(position_list,matrix_all[,"named_position"]),]
+  
+  }
   
   if(position_to_rownames){
     
