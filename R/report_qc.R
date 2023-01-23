0b55e8b0edf180863befacfb005c443e5d91e925
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2023-01-23 15:27:56 +0100

    Replace paste0 with file.path

--- R/report_qc.R
+++ R/report_qc.R
@@ -9,7 +9,7 @@ report_qc <- function(ribo, library_col, project_name = "", output_dir = getwd()
     ribo_name <- deparse(substitute(ribo))
     ribo_to_check <- ribo #used in Rmarkdown template # nolint
     path <- system.file("rmd", package = "rRMSAnalyzer")
-    rmarkdown::render(paste0(path, "/check_bias.Rmd"),
+    rmarkdown::render(file.path(path, "check_bias.Rmd"),
                       params = list(
                                     library_col = library_col,
                                     ribo_name = ribo_name,
