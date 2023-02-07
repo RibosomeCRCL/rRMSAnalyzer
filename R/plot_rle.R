9cadfa3757a1a688df0623c24b6374ece4057999
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2023-02-07 15:54:04 +0100

    plot_RLE added

---
+++ R/plot_rle.R
@@ -0,0 +1,71 @@
+
+#' Compute Relative Log Expression (RLE)
+#'
+#' @param mat Data matrix to compute RLE on
+#'
+#' @return a RLE matrix
+#' @keywords internal
+#' 
+.compute_RLE <- function(mat = NULL){
+  stopifnot(!is.null(mat))
+  mat <- mat + 1
+  med <- apply(mat, 1, median)
+  rle <- log2(mat/med)
+  rle <- tidyr::gather(as.data.frame(rle))
+  return(rle)
+}
+
+
+#' (internal) Compute and plot Relative Log Expression for a chosen data in as RiboClass
+#'
+#' @param mat matrix
+#' @param show_outlier Show outliers on the plot ?
+#' @param col_to_plot  data column to plot values from
+#'
+#' @return a ggplot object
+#' @keywords internal
+#' 
+.plot_rle <- function(mat = NULL,show_outlier,col_to_plot) {
+  
+  # Non Standard Evaluation warning fix
+  key <- NULL
+  value <- NULL
+  
+  stopifnot(!is.null(mat))
+  mat <- as.data.frame(mat)
+  rle_calc <- .compute_RLE(mat = mat)
+  outlier_shape=NA
+  if(show_outlier) outlier_shape = 19
+  mad <- - 2*mad(rle_calc$value, na.rm = T)
+  rle_grouped <- rle_calc %>% dplyr::group_by(key) %>% dplyr::summarise(median = stats::median(value, na.rm = TRUE))
+  
+  rle.plot <- ggplot2::ggplot(rle_calc, ggplot2::aes(x = key, value)) + 
+    ggplot2::geom_boxplot(outlier.shape = outlier_shape, outlier.size = 0.3,
+                 fill = ifelse(rle_grouped$median < mad, "red","white")) +
+    ggplot2::ylab(paste0("RLE")) +
+    ggplot2::xlab("Sample") +
+    ggplot2::ggtitle("RLE plot")+
+    ggplot2::geom_hline(yintercept = 2*mad(rle_calc$value, na.rm = T), colour = "blue") + 
+    ggplot2::geom_hline(yintercept = - 2*mad(rle_calc$value, na.rm = T), colour = "blue") +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(
+      colour = ifelse(rle_grouped$median < mad,"red","black"),angle = 90))
+ 
+   return(rle.plot)
+}
+
+#' Relative Log Expression plot of a RiboClass object’s counts. 
+#'
+#' @param ribo a RiboClass object.
+#' @param outlier Show boxplot outlier values. 
+#'
+#' @return A ggplot object. Samples with a median lower than median(RLE counts)-2*MAD (Median Absolute Deviation) are colored in red.
+#' @export
+#'
+#' @examples 
+#' data("ribo_toy")
+#' plot_rle(ribo_toy)
+plot_rle <- function(ribo, show_outlier=TRUE) {
+  
+  rle_matrix <- extract_data(ribo, col = "count",position_to_rownames = T)
+  return(.plot_rle(rle_matrix,show_outlier = show_outlier,"count"))
+}
