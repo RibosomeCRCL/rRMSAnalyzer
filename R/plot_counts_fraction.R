468d0bcdcc0ac27040a2c14f4b2636c59188a3b4
hermespara <hermes.paraqindes@etu.univ-lyon1.fr>
2023-03-09 10:25:56 +0100

    Add new plot fonction for counts fractions per rna and sample

---
+++ R/plot_counts_fraction.R
@@ -0,0 +1,23 @@
+
+plot_counts_fraction <- function(ribo = NULL) {
+  #output
+  # faire les check
+  all_data <- dplyr::bind_rows(ribo$data, .id = "samplesid")
+  
+  all_data_sums <- all_data %>%
+    group_by(samplesid, rna) %>%
+    summarise(sum.counts = sum(count, na.rm = T), n0 = length(which(count < 5)))
+  
+  all_data_sums$rna <- factor(all_data_sums$rna, levels = ribo$rna_names$current_name)
+  
+  counts_plot <- ggplot(all_data_sums, aes(x = samplesid, y = sum.counts, fill = rna))
+  
+  counts_plot <- counts_plot + geom_bar(stat = "identity", position = "fill") +
+    scale_fill_brewer(palette = "Spectral") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, size = 7, hjust = 1)) +
+    labs(title = "Counts distribution per sample", 
+         x = "Sample",
+         y = "Counts fraction")
+  return(counts_plot)
+}
