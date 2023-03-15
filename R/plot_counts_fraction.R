7c85f9787214954562830f8da692c91b0d0b602f
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2023-03-15 14:15:54 +0100

    Improvements and doc for plot_counts_fraction
    Use native pipe instead of maggritr's. Code formatting

--- R/plot_counts_fraction.R
+++ R/plot_counts_fraction.R
@@ -1,23 +1,33 @@
+#' Plot count distribution among RNAs for each sample
+#'
+#' @param ribo A RiboClass object.
+#'
+#' @return a ggplot object
+#' @export
+#'
+#' @examples
+#' data("ribo_toy")
+#' plot_counts_fraction(ribo_toy)
+plot_counts_fraction <- function(ribo) {
 
-plot_counts_fraction <- function(ribo = NULL) {
-  #output
-  # faire les check
   all_data <- dplyr::bind_rows(ribo$data, .id = "samplesid")
   
-  all_data_sums <- all_data %>%
-    group_by(samplesid, rna) %>%
-    summarise(sum.counts = sum(count, na.rm = T), n0 = length(which(count < 5)))
+  all_data_sums <- all_data |>
+    dplyr::group_by(samplesid, rna) |>
+    dplyr::summarise(sum.counts = sum(count, na.rm = T),
+                     n0 = length(which(count < 5)))
   
   all_data_sums$rna <- factor(all_data_sums$rna, levels = ribo$rna_names$current_name)
   
   all_data_sums$samplesid <- factor(all_data_sums$samplesid, levels = names(ribo[["data"]]))
   
-  counts_plot <- ggplot(all_data_sums, aes(x = samplesid, y = sum.counts, fill = rna))
+  counts_plot <- ggplot2::ggplot(all_data_sums, aes(x = samplesid, y = sum.counts, fill = rna))
   
-  counts_plot <- counts_plot + geom_bar(stat = "identity", position = "fill") +
-    scale_fill_brewer(palette = "Spectral") +
-    theme_bw() +
-    theme(axis.text.x = element_text(angle = 45, size = 7, hjust = 1)) +
+  counts_plot <- counts_plot + ggplot2::geom_bar(stat = "identity",
+                                                 position = "fill") +
+    ggplot2::scale_fill_brewer(palette = "Spectral") +
+    ggplot2::theme_bw() +
+    ggplot2::theme(axis.text.x = element_text(angle = 45, size = 7, hjust = 1)) +
     labs(title = "Counts distribution per sample", 
          x = "Sample",
          y = "Counts fraction")
