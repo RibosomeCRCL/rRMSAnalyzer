e5482ef423e8244da25ea71614c39d8b404afb1a
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2023-04-07 10:23:26 +0200

    create_dt function to generate downloadable DT

--- R/utils.R
+++ R/utils.R
@@ -5,3 +5,15 @@ check_metadata <- function(ribo,metadata_name) {
          paste(colnames(ribo[["metadata"]]),collapse = ", "))
   }
 }
+
+# Adapted from: https://martinctc.github.io/blog/vignette-downloadable-tables-in-rmarkdown-with-the-dt-package/
+create_dt <- function(x){
+  DT::datatable(x,
+                rownames = FALSE,
+                extensions = 'Buttons',
+                options = list(dom = 'Blfrtip',
+                               buttons = list('copy', 'csv', list(
+                                 extend='excel',
+                                 text="Excel"), 'pdf')
+                               ))
+}
