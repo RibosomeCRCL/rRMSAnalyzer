ce0aed57c80e7b99af1b1d7691318750cdad6c20
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2023-04-11 15:30:33 +0200

    Use custom annotation instead of loading human_methylated

--- tests/testthat/test_annotate.R
+++ tests/testthat/test_annotate.R
@@ -2,10 +2,16 @@ test_that("Annotation works correctly",{
   
   data("ribo_toy")
   
-  ribo_toy <- rRMSAnalyzer::rename_rna(ribo_toy)
+  # We will create a custom annotation just for this test
   
-  data("human_methylated")
-  ribo_toy <- rRMSAnalyzer::annotate_site(ribo_toy,human_methylated)
+  custom_anno <- data.frame(
+    rnapos = c(15,76,100,401),
+    rna = c("5.8S","5.8S","18S","28S"),
+    nomenclature = c("5.8S_Um14","5.8S_Gm75","18S_Am99","28S_Am391")
+  )
+  
+  ribo_toy <- rRMSAnalyzer::rename_rna(ribo_toy)
+  ribo_toy <- rRMSAnalyzer::annotate_site(ribo_toy,custom_anno)
   ribom <- rRMSAnalyzer::extract_data(ribo_toy,only_annotated = T)
   
   expected_ribom <- readRDS(testthat::test_path("testdata","anno.rds"))
