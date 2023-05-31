6b94d77743e30bb727396c137d153dbcec5698b5
TheoC_SLC <Theo.COMBE@lyon.unicancer.fr>
2023-05-31 14:46:22 +0200

    cli_abort for remove and keep samples

--- R/keep_or_remove_samples_ribo.R
+++ R/keep_or_remove_samples_ribo.R
@@ -12,14 +12,10 @@
 
 keep_ribo_samples <- function(ribo = NULL, samples_to_keep = NULL) {
   
-  if(all(samples_to_keep %in% names(ribo[["data"]]))) {
-    
+    check_sample(ribo,samples_to_keep)
     ribo[["data"]] <- ribo[["data"]][samples_to_keep]
     ribo[["metadata"]] <- ribo[["metadata"]][match(samples_to_keep, rownames(ribo[["metadata"]])),]
     
-  } else {
-    stop("Samples name should be from ", toString(names(ribo[["data"]])))
-  }
   return(ribo)
 }
 
@@ -37,14 +33,11 @@ keep_ribo_samples <- function(ribo = NULL, samples_to_keep = NULL) {
 #' ribo_test <- remove_ribo_samples(ribo = ribo_toy, samples_to_delete = c("S1", "S2"))
 
 remove_ribo_samples <- function(ribo = NULL, samples_to_delete = NULL) {
+  check_sample(ribo,samples_to_delete)
   
-  if(all(samples_to_delete %in% names(ribo[["data"]]))) {
-    idx <- match(samples_to_delete, names(ribo[["data"]]))
-    ribo[["data"]] <- ribo[["data"]][-idx]
-    ribo[["metadata"]] <- ribo[["metadata"]][-match(samples_to_delete, rownames(ribo[["metadata"]])),]
+  idx <- match(samples_to_delete, names(ribo[["data"]]))
+  ribo[["data"]] <- ribo[["data"]][-idx]
+  ribo[["metadata"]] <- ribo[["metadata"]][-match(samples_to_delete, rownames(ribo[["metadata"]])),]
     
-  } else {
-    stop("Samples name should be from ", toString(names(ribo[["data"]])))
-  }
   return(ribo)
 }
--- R/utils.R
+++ R/utils.R
@@ -5,6 +5,7 @@
 #' the vector are not valid metadata.
 #' @param ribo A RiboClass.
 #' @param metadata_name The vector of string to check against ribo's metadata.
+#' @keywords internal
 #'
 check_metadata <- function(ribo,metadata_name) {
   # Get metadata columns that do not exist in ribo's metadata.
@@ -15,12 +16,36 @@ check_metadata <- function(ribo,metadata_name) {
   if(len_unmatched > 0) {
     
     cli::cli_abort(c(
-      "You have supplied names that are not part of the RiboClass metadata",
+      "You have supplied names that are not part of the RiboClass metadat.",
       "i" = "The RiboClass has the following metadata:
-      {.val {colnames(ribo[['metadata']])}}",
+      {.val {colnames(ribo[['metadata']])}}.",
       "x" = "{len_unmatched} supplied name{?s} {?is/are} not part of the metadata:
-      {.val {unmatched_elts}}"
+      {.val {unmatched_elts}}."
     ))
     
+  }
+}
+#' Check if a vector of strings correspond to valid samplenames in a given RiboClass
+#'
+#' This function returns nothing if each string matches with the samplenames
+#' It will stop and display a formatted error message if one or more strings in
+#' the vector are not valid samplenames
+#' @param ribo A RiboClass.
+#' @param sample_names The vector of string to check against ribo's samplenames
+#' @keywords internal
+#'
+check_sample <- function(ribo, sample_names) {
+  unmatched_elts <- sample_names[
+    which(!(sample_names %in% names(ribo[["data"]])))
+  ]
+  len_unmatched <- length(unmatched_elts)
+  
+  if(len_unmatched > 0) {
+    cli::cli_abort(c(
+      "You have supplied samplenames that are not part of the supplied RiboClass",
+      "i" = "Sample names in RiboClass : {.val {names(ribo[['data']])}}.",
+      "x" = "{len_unmatched} supplied name{?s} {?is/are} not part of the RiboClass' samplenames:
+      {.val {unmatched_elts}}."
+    ))
   }
 }
