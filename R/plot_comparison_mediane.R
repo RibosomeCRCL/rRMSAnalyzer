cce37cd6cb2b8a61e3d794e89dbbeed908b8b717
Allyson <allyson.mrx.com>
2025-05-28 13:53:11 +0200

    chache generate_palette car fonction interne

--- R/plot_comparison_mediane.R
+++ R/plot_comparison_mediane.R
@@ -12,16 +12,11 @@
 #' plot_comparison_medianes(ribo_adj_annot, params$condition_col, params$ech, params$ctrl)
 
 plot_comparison_medianes <- function(ribo = NULL, condition_col = NULL,  ech = NULL, ctrl = NULL) {
-  # if (lentgh(unique(condition_col)) > 2) {
-  #   stop("too much modality in condition")}
-  #   else {
-  #     
+  
   ribo_df <- extract_data(ribo_adj_annot,
                         col = "cscore",
                         only_annotated = TRUE)
 # 7.1. transforming df
-
-library(dplyr)
 
 cond1 <- ribo$metadata %>% #filter sample of your 1st condition 
   dplyr::filter(!!sym(condition_col) == paste0(ech)) 
@@ -35,33 +30,46 @@ new_metadata_sites <- ribo_df %>%
   tidyr::pivot_longer(cols = -site,         # All columns but "site"
                names_to = "samplename", 
                values_to = "cscore") %>%
-  dplyr::filter(samplename %in% cond1_2)  # Filtrer only sample present in cond1 et cond2
+  dplyr::filter(samplename %in% cond1_2)  # Filter only sample present in cond1 et cond2
 
 
-metadata_data_total <- new_metadata_sites%>%
+metadata_data_total <- new_metadata_sites %>%
   dplyr::group_by(site) %>%
   dplyr::summarize(median_Cscore_metadata_ech_ctrl = median(cscore[samplename %in% cond1$samplename]), # take samplename into cond1
             median_Cscore_metadata_ech = median(cscore[samplename %in% cond2$samplename]), # take samplename into cond2
             difference = median_Cscore_metadata_ech - median_Cscore_metadata_ech_ctrl) # compute the difference between both
 
 
+
 metadata_data_total <- metadata_data_total %>%
   dplyr::mutate(site = as.character(site))
 
 # keep site order (to debug)
-metadata_data_total <- metadata_data_total %>%
-  dplyr::mutate(site = factor(site, levels = rev(unique(human_methylated$Nomenclature))))
+# metadata_data_total <- metadata_data_total %>%
+#   dplyr::mutate(site = factor(site, levels = rev(unique(human_methylated$Nomenclature))))
 
-# create difference_cat column
-metadata_data_total$difference_cat <- ifelse(abs(metadata_data_total$difference) < 0.05, "No difference", 
+#create difference_cat column
+metadata_data_total$difference_cat <- ifelse(abs(metadata_data_total$difference) < 0.05, "No difference",
                                              ifelse(metadata_data_total$difference > 0.05, "Increase",
                                                     ifelse(metadata_data_total$difference < 0.05, "Decrease", "ok")))
 
+# metadata_data_total <- metadata_data_total %>%
+#   dplyr::mutate(
+#     difference_cat = dplyr::case_when(
+#       abs(difference) < 0.05 ~ "No difference",
+#       difference >= 0.05     ~ "Increase",
+#       difference <= -0.05    ~ "Decrease"
+#     ),
+#     difference_cat = factor(difference_cat, levels = c("Increase", "No difference", "Decrease"))
+#   )
+
 # create a color vector for tags according to difference_cat
-site_colors <- ifelse(metadata_data_total$difference_cat == "Increase", "red", 
+site_colors <- ifelse(metadata_data_total$difference_cat == "Increase", "red",
                       ifelse(metadata_data_total$difference_cat == "Decrease", "blue", "black"))
-# /!\ Vectorized input to `element_text()` is not officially supported.
+# # /!\ Vectorized input to `element_text()` is not officially supported.
 # ℹ Results may be unexpected or may change in future versions of ggplot2. 
+
+#site_colors <- c("Increase" = "red", "No difference" = "black", "Decrease" = "blue")
 
 #7.3. Graphique n°1: vizualisation of absolute cscore value
 part1 <- ggplot(metadata_data_total, aes(x=site, y=difference, label=site)) +
