b46ef92a6a5ccb53f6e05aeccaa49e0cbb95469b
Allyson <allyson.mrx.com>
2025-06-20 09:56:40 +0200

    translation in english

--- Model.R
+++ Model.R
@@ -1,47 +1,42 @@
 #----------------------------------------------------------------------------------
 #
-#   rRMSAnalyzer: script d'analyse de RiboMethSeq | Humain et Murin
-#   par VM le 22/06/2023
-#   à l'aide des packages rRMSAnalyzer & rRMSReports & rRMSFonctions
+#   rRMSAnalyzer: analysis script of RiboMethSeq | Human et Murine
+#   by llyson Moureaux inspired by Virginie Marcel the 19/06/2025
+#   thankes to rRMSAnalyzer & rRMSReports packages & rRMSFonctions
 #
-#   Pour de l'aide plus spécifique: https://ribosomecrcl.github.io/rRMSAnalyzer/
+#   For more specific help: https://ribosomecrcl.github.io/rRMSAnalyzer/
 #
 #----------------------------------------------------------------------------------
 
 #----------------------------------------------------------------------------------
-# 0. Manipulation sous R préalable
+# 0. Before starting analysis
 #----------------------------------------------------------------------------------
 
 
-#0.0. Créer un nouveau projet et préparation de l'environnement de travail
+#0.0. Create a new Project and prepare working environment
 
-#Pour des premières prises en main: aller directment à #0.1 et utiliser le ribo_toy puis créer un riboClass à partir des données de 5FU fournies
-
-#Pour analyser ses propres données:
-#Aller dans /file/new projet/ et créer un nouveau dossier
-#Dans ce nouveau dossier, copier/coller les fichiers du dossier "rRMSAnalyser_template":
+#go to /file/new project/ create a new folder
+#In this folder, copy and paste files of directory "rRMSAnalyser_template":
 #metadata.csv + rRMSAnalyzer_template.R
-#si c'est une analyse souris, prendre aussi "mouse.methylated.rda" et "mouse.suspected.rda"
-#De plus, n'oublier pas de
-#copier/coller le dossier avec les 5'end counts
-#ouvrir le metadata.csv sous Excel,  remplir le fichier puis l'enregistrer en .csv avec séparation en ";" dans le dossier de comptage
+#if it's mouse analysis take "mouse.methylated.rda" and "mouse.suspected.rda"
+#Don't forget to put your 5'end counts file in this directory.
+#open metadata.csv in Excel, fill in the file and save it under .csv with ";" as separator.
 
 
-#0.1. Mise à jour des packages
+#0.1. Package update
 library(devtools)
 
-#0.1.1. RMSAnalyzer
+#0.1.1. RMSAnalyzer and RMSReport
 devtools::install_github("RibosomeCRCL/rRMSAnalyzer")
 
 library(rRMSAnalyzer)
 library(rRMSReports)
 
 #----------------------------------------------------------------------------------
-# 1. Création du RiboClass
+# 1. RiboClass creation
 #----------------------------------------------------------------------------------
 
-
-# 1.2. Préparation d'un jeu de données personnelles.
+# 1.2. Personal data preparation
 
 ribo <- load_ribodata(
   #data & metadata files path
@@ -62,8 +57,8 @@ ribo <- load_ribodata(
   method = "median",
   ncores = 1)
 
-# Vérification de l'importation de données avec PCA
-plot_pca(ribo,"lib") #changer le nom de la colonne en fonction du message d'erreur
+# Check importation with a PCA
+plot_pca(ribo,"lib") #change column name of the library if necessary
 
 #Plot coa
 plot_coa(
@@ -76,14 +71,14 @@ plot_coa(
   object_only = FALSE
 )
 #----------------------------------------------------------------------------------
-# 2. Etape de QC
+# 2. QC step
 #----------------------------------------------------------------------------------
 
-# 2.1. Réalisation du QC_report pour toutes les données
+# 2.1. QC_report for all data
 
 report_qc(ribo = ribo, library_col = "lib")
 
-# 2.2. Ajustement par ComBat-Seq si nécessaire
+# 2.2. adjustment ComBat-Seq if necessary
 
 plot_pca(ribo,"library")                #ribo_toy library_col = "run"
 
@@ -91,21 +86,21 @@ ribo_adj <- adjust_bias(ribo,"library")
 
 plot_pca(ribo_adj,"library")            
 
-# 2.3. Réalisation du QC_report pour toutes les données ajustées
+# 2.3. QC_report for adjusted data
 
 report_qc(ribo = ribo_adj, library_col = "library", project_name = "xxx", comments = "./commentaire_QC.Rmd") 
 
 #----------------------------------------------------------------------------------
-# 3. Retirer les échantillons problématiques ou qui ne font pas partis de l'étude
+# 3. Remove problematic or not used samples
 #----------------------------------------------------------------------------------
 
-# 3.1. Garder un faible nombre d'échantillons.
+# 3.1. Keep a small number of samples
 ribo_adj_small <- keep_ribo_samples(ribo_adj,c("sample1","sample2","sample3","sample4"))
                                  
-# 3.1.bis Enlever un faible nombre d'échantillons                                
+# 3.1.bis  remove a small number of samples                        
 ribo_adj_small <- remove_ribo_samples(ribo_adj,c("xxxx","xxxccc","RNA_ref-1","RNA_ref-2"))
 
-# 3.3. Uniformiser le nom du riboClass de cette étape
+# 3.3. Uniformisation of Riboclass name
 ribo_adj <- ribo_adj_small 
 
 
@@ -115,19 +110,19 @@ ribo_adj <- ribo_adj_small
 
 data("human_methylated")
 cat("human_methylated's rna names: ", unique(human_methylated$rRNA),"\n")
-cat("ribo's rna names: ", as.character(ribo$rna_names$current_name)) # changer le nom du RiboClass
+cat("ribo's rna names: ", as.character(ribo$rna_names$current_name)) # change name of RiboClass if necessary
 
-ribo_adj_name <- rename_rna(ribo,
+ribo_adj_name <- rename_rna(ribo, #change name of RiboClass if necessary for ribo_adj if you adjusted the data
                             new_names = c("5S", "5.8S", "18S", "28S"))
 
-ribo_adj_annot <- annotate_site(ribo_adj_name,
+ribo_adj_annot <- annotate_site(ribo_adj_name, 
                                 annot = human_methylated,
                                 anno_rna = "rRNA",
                                 anno_pos = "Position",
                                 anno_value = "Nomenclature")
 
 #----------------------------------------------------------------------------------
-# 4B. dans un deuxième temps, refaire toute l'analyse avec les sites suspectés si besoin
+# 4B. redo all the analysis for adjusted data if necessary (replace step 4A then ABis by 4B then BBis)
 #----------------------------------------------------------------------------------
 
 load(file = "/home/bioinfo/Bureau/RMS/mouse.suspected.rda") 
@@ -142,7 +137,7 @@ ribo_adj_annot <- annotate_site(ribo_adj_annot,
                                 anno_value = "Nomenclature")
 
 #----------------------------------------------------------------------------------
-# 4Abis. Annotation des sites de 2'Ome des ARNr MURINS
+# 4Abis. 2'Ome sites of MURINS rRNA annotation
 #------------------------------------
 
 load(file = "/home/bioinfo/Bureau/RMS/mouse.methylated.rda") 
@@ -156,7 +151,7 @@ ribo_adj_annot <- annotate_site(ribo_adj_annot,
                                 anno_pos = "Position",
                                 anno_value = "Nomenclature")
 #------------------------------------
-# 4Bis. dans un deuxième temps, refaire toute l'analyse avec les sites suspectés si besoin
+# 4Bis. redo all the analysis for adjusted data if necessary (replace step 4A then ABis by 4B then BBis)
 #------------------------------------
 
 load(file = "/home/bioinfo/Bureau/RMS/mouse.suspected.rda") 
@@ -171,12 +166,12 @@ ribo_adj_annot <- annotate_site(ribo_adj_annot,
                                 anno_value = "Nomenclature")
 
 #----------------------------------------------------------------------------------
-# 5. Rapports automatiques
+# 5. Reports
 #----------------------------------------------------------------------------------
 
-# A refaire autant de fois qu'il y a de variables (A, B, C ...). Ex de metadata :
+# To launch as many time as the number of comparisons asked(A, B, C ...). Ex of metadata:
 
-# filename	              samplename	      variableA	  variableB	    variableC	  condition	  traitement	lib
+# filename	              samplename	      variableA	  variableB	    variableC	  condition	  treatment	  lib
 # S26_R1.5_counts.csv     variableB_N2_P2	  NA	        P2	          NA	        variableB	  P2	        L2
 # S27_R1.5_counts.csv	    variableA_N2_P1	  P1	        NA	          NA	        variableA	  P1	        L2
 # S28_R1.5_counts.csv	    variableC_D4_ctrl	NA	        NA	          ctrl	      variableC	  ctrl	      L2
@@ -197,61 +192,61 @@ ribo_adj_annot <- annotate_site(ribo_adj_annot,
 # S44_R1.5_counts.csv	    variableB_N3_ctrl	NA	        ctrl	        NA	        variableB	  ctrl        L2
 # S45_R1.5_counts.csv	    variableA_N3_P2	  P2	        NA	          NA	        variableA	  P2	        L2
 
-# Si vous n'avez qu'une seule colonne "condition" le fonctionnement est le même. 
+# If only one column condition, it is still the same thing.
 # --------------------------------------A------------------------------------------
 
-# 5.1 filtrer avec les métadonnées pour ne garder que les échantillons présents dans la colonne "variableA"
+# 5.1 filter with metadata to keep only samples present in the colum "variableA"
 kept_samples <- ribo_adj_annot$metadata %>%
-  dplyr::filter(!is.na(variableA)) %>% # ne garde que les lignes qui ne sont pas rempli avec "NA"
+  dplyr::filter(!is.na(variableA)) %>% # keep lines that are not "NA"
   dplyr::pull(samplename)
 
 ribo_adj_annot_variableA <- keep_ribo_samples(ribo_adj_annot,kept_samples)
 
-# créer le tableau de comparaison nécessaire pour le rapport diff_sites
+# create the necessary comparison table for the diff_sites report
 comparisons <- tibble::tibble(
   comp = c("comp1", "comp2"),
   ctrl = c("ctrl", "ctrl"),
   cases = c("P1", "P2")
 )
 
-# 5.2. Analyse du profil global de 2'Ome des ARNr et des xx sites les plus variants
+# 5.2. 2'Ome of rRNAs analysis of the global profile  and the xx most variant sites
 report_2ome_sites(ribo_adj_annot_variableA,"variableA", project_name = "nom", comments = "./commentaire_2ome_variableA.Rmd") 
 
-# 5.3. Analyse des différences de niveau de 2'Ome des ARNr
+# 5.3. Analysis of rRNAs 2'Ome level differences
 report_diff_sites(ribo_adj_annot_variableA, condition_col = "variableA", project_name = "nom", comparisons = comparisons, comments = "./commentaire_diff_site_variableA.Rmd") 
 
 # --------------------------------------B------------------------------------------
 
-# 5.1 filtrer avec les métadonnées pour ne garder que les échantillons présents dans la colonne "variableB"
+# 5.1 filter with metadata to keep only samples present in the colum "variableB"
 kept_samples <- ribo_adj_annot$metadata %>%
   dplyr::filter(!is.na(variableB)) %>%
   dplyr::pull(samplename)
 
 ribo_adj_annot_variableB <- keep_ribo_samples(ribo_adj_annot,kept_samples)
 
-# créer le tableau de comparaison nécessaire pour le rapport diff_sites
+# create the necessary comparison table for the diff_sites report
 comparisons <- tibble::tibble(
   comp = c("comp1"),
   ctrl = c("ctrl"),
   cases = c("P2")
 )
 
-# 5.2. Analyse du profil global de 2'Ome des ARNr et des xx sites les plus variants
+# 5.2. 2'Ome of rRNAs analysis of the global profile  and the xx most variant sites
 report_2ome_sites(ribo_adj_annot_variableB,"variableB", project_name = "nom", comments = "./commentaire_2ome_variableB.Rmd") 
 
-# 5.3. Analyse des différences de niveau de 2'Ome des ARNr
+# 5.3.  Analysis of rRNAs 2'Ome level differences
 report_diff_sites(ribo_adj_annot_variableB, condition_col = "variableB", project_name = "nom", comparisons = comparisons, comments = "./commentaire_diff_site_variableB.Rmd") 
 
 # --------------------------------------C------------------------------------------
 
-# 5.1 filtrer avec les métadonnées pour ne garder que les échantillons présents dans la colonne "variableC"
+# 5.1 filter with metadata to keep only samples present in the colum "variableC"
 kept_samples <- ribo_adj_annot$metadata %>%
   dplyr::filter(!is.na(variableC)) %>%
   dplyr::pull(samplename)
 
 ribo_adj_annot_variableC <- keep_ribo_samples(ribo_adj_annot,kept_samples)
 
-# créer le tableau de comparaison nécessaire pour le rapport diff_sites
+# create the necessary comparison table for the diff_sites report
 comparisons <- tibble::tibble(
   comp = c("comp1"),
   ctrl = c("ctrl"),
@@ -259,33 +254,33 @@ comparisons <- tibble::tibble(
 )
 
 
-# 5.2. Analyse du profil global de 2'Ome des ARNr et des xx sites les plus variants
+# 5.2. 2'Ome of rRNAs analysis of the global profile  and the xx most variant sites
 report_2ome_sites(ribo_adj_annot_variableC,"variableC", project_name = "nom", comments = "./commentaire_2ome_variableC.Rmd") 
 
-# 5.3. Analyse des différences de niveau de 2'Ome des ARNr
+# 5.3.  Analysis of rRNAs 2'Ome level differences
 report_diff_sites(ribo_adj_annot_variableC, condition_col = "variableC", project_name = "nom", comparisons = comparisons, comments = "./commentaire_diff_site_variableC.Rmd") 
 
 
 #----------------------------------------------------------------------------------
-# 6. Vérifier les environnements locaux des sites d'intérêts
+# 6. Check local environments of sites of interest
 #----------------------------------------------------------------------------------
 
 
-# 6.1 Pour tous les échantillons
+# 6.1 For all samples
 plot_counts_env(ribo_adj_annot, rna= "18S", pos = 1442, samples = "all", condition = "condition")
 
-# 6.2 Pour certain échantillons
+# 6.2 For some samples
 plot_counts_env(ribo_adj_annot, rna = "18S", pos = 1442, samples = c("sample1", "sample2"))
 
 
 #----------------------------------------------------------------------------------
-# 7. Export des données
+# 7. data export
 #----------------------------------------------------------------------------------
 
 ribo_df <- extract_data(ribo_adj_annot,
                         col = "cscore",
                         only_annotated = TRUE)
 
-write.csv(ribo_df, "/chemin/vers/dossier/ribo_df.csv") #indiquer le chemin
+write.csv(ribo_df, "/chemin/vers/dossier/cscore_df.csv") #give path
 
 #----------------------------------------------------------------------------------
