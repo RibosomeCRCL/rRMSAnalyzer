ca3700f7c278337ef1829d35feeb667d33cdcdaf
Allyson <allyson.mrx.com>
2025-07-04 15:14:14 +0200

    shorten variable name

--- R/plot_global_profile.R
+++ R/plot_global_profile.R
@@ -10,13 +10,13 @@
 #' @examples
 #' plot_global_profile(ribo, condition_col, ech, ctrl)
 
-plot_global_profile <- function(ribo = ribo_adj_annot, condition_col = NULL) { #, ech = NULL, ctrl = NULL) {
+plot_global_profile <- function(ribo = ribo_adj_annot, condition_col = NULL) { 
 
   #Check for ribo
   if (is.null(ribo)) {stop("MISSING parameter: please provide a RiboClass!")}
   if (!inherits(ribo, "RiboClass")) {stop("ribo argument is not a RiboClass!")}
   
-  metadata_x <- ribo$metadata # extract metadata from ribo object
+  meta <- ribo$metadata # extract metadata from ribo object
   ribom <- extract_data(ribo, col = "cscore", only_annotated = TRUE, position_to_rownames = TRUE)
   
   # Transform ribom into long format excluding -annotated_sites
@@ -26,7 +26,7 @@ plot_global_profile <- function(ribo = ribo_adj_annot, condition_col = NULL) { #
 
 # fusion with metadata
   ribom_merged <- ribom_long %>% 
-    dplyr::left_join(metadata_x, by = "samplename") # left join between metadata and ribom_long 
+    dplyr::left_join(meta, by = "samplename") # left join between metadata and ribom_long 
   
   result <- ribom_merged %>%
     dplyr::group_by(annotated_sites, !!sym(condition_col)) %>%
@@ -35,11 +35,20 @@ plot_global_profile <- function(ribo = ribo_adj_annot, condition_col = NULL) { #
               .groups = "drop") 
   
   #keep order as in human methylated 
-  result <- result %>% 
-    dplyr::mutate(annotated_sites = factor(annotated_sites, 
-                                    levels = unique(human_methylated$Nomenclature))) %>%
-    dplyr::arrange(annotated_sites)
+  #first order
+  fst_order <- (unique(human_methylated$Nomenclature))
+  
+  # second order
+  other_sites <- setdiff(unique(result$annotated_sites), fst_order) # sites only present in result 
+  
+  #concatenation of both order
+  final_order <- c(other_sites, fst_order) #order 5.8S before 18 and 28-S
+  
+  #use final_order
+  result <- result %>%
+    dplyr::mutate(annotated_sites = factor(annotated_sites, levels = final_order)) 
 
+  # draw graph
   if (length(unique(condition_col)) > 4) stop("Too many conditions to draw a clear plot.")
     
     global_profile <- ggplot(result, aes(x = annotated_sites, y = mean_c_score , group = !!sym(condition_col) , color = !!sym(condition_col), fill = !!sym(condition_col))) +
@@ -48,13 +57,13 @@ plot_global_profile <- function(ribo = ribo_adj_annot, condition_col = NULL) { #
       scale_x_discrete(guide = guide_axis(angle = 90)) +
       scale_y_continuous(limits = c(0, 1)) +
       labs(
-        x = "RNA 2'Ome Methylation sites", y = "C-score",
+        x = "rRNA 2'Ome sites", y = "C-score",
         color = condition_col, fill = condition_col,
-        title = "2'Ome Global Profile"
+        title = "Variation in 2'Ome levels between conditions"
       ) +
       theme_bw() +
-      scale_color_brewer(palette = "Accent") +
-      scale_fill_brewer(palette = "Accent")
+      scale_color_brewer(palette = "Accent", name = "Condition") +
+      scale_fill_brewer(palette = "Accent", name = "Condition")
     
     return(global_profile)
   }
