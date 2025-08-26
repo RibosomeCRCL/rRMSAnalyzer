069aeb3dc993169727040cd7900c32af24611c9d
Allyson <allyson.mrx.com>
2025-08-26 15:26:11 +0200

    3 new function for diff_sites. Compute and displays statistics

---
+++ R/get_diff_sites_summary.R
@@ -0,0 +1,154 @@
+#' Get a summary of statistics for differential 2'ome-sites analysis
+#'
+#' @param ribo a RiboClass
+#' @param pthr p-value threshold
+#' @param condition_col the condition column used for calculation in metadata
+#' @param cscore_cutoff cutoff of c-score under which sites are not taken in account
+#' @param comparisons table given by the user
+#'
+#' @returns a data frame
+#' @export
+#'
+#' @examples
+#' data("ribo_toy")
+#' data("human_methylated")
+#' ribo_toy <- remove_ribo_samples(ribo_toy,c("RNA1", "RNA2"))
+#' ribo_toy <- rename_rna(ribo_toy)  
+#' ribo_toy <- annotate_site(ribo_toy,
+#'                                 annot = human_methylated,
+#'                                 anno_rna = "rRNA",
+#'                                 anno_pos = "Position",
+#'                                 anno_value = "Nomenclature")
+#' comparisons <- tibble::tibble(
+#'                               comp = c("comp1"),
+#'                               ctrl = c("cond1"),
+#'                               cases = c("cond2") )                                
+#' get_diff_sites_summary(ribo = ribo_toy, pthr = 0.05, condition_col = "condition",
+#'  cscore_cutoff = 0.05, comparisons = comparisons)
+                      
+get_diff_sites_summary <- function(ribo = ribo, pthr = 0.05, condition_col = condition_col, cscore_cutoff = 0.05, comparisons = comparisons){
+  
+  #extract metadata
+  qcdata <- ribo[[2]] 
+  
+  #----------------------------------------ANOVA test---------------------------
+  if(length(unique(qcdata$condition_col)) > 2){
+  res_pv_anova <- rRMSAnalyzer::res_pv(ribo = ribo, test = "anova", condition_col = condition_col) 
+  a <-rRMSAnalyzer::plot_stat(ribo = ribo, res_pv = res_pv_anova, pthr = pthr, condition_col = condition_col, cscore_cutoff = cscore_cutoff)
+  }
+  
+  #----------------------------------------Welch test---------------------------
+  w_list <- list() 
+  for(i in seq_len(nrow(comparisons))) {
+    ctrl <- comparisons$ctrl[i]
+    cases <- comparisons$cases[i]
+    
+    ribo_filtered <- ribo
+    kept_samples <- ribo$metadata %>%
+      dplyr::filter(!!sym(condition_col) %in% c(cases, ctrl))%>%
+      dplyr::pull(samplename)
+    
+    ribo_filtered <- keep_ribo_samples(ribo_filtered,kept_samples)
+    
+    res_pv_welch <- rRMSAnalyzer::res_pv(ribo = ribo_filtered, test = "student", condition_col = condition_col) 
+    
+    w <- rRMSAnalyzer::plot_stat(ribo = ribo_filtered, res_pv = res_pv_welch, pthr = pthr, condition_col = condition_col, cscore_cutoff = cscore_cutoff)
+    w_list[[i]] <- w 
+  }
+  
+  #----------------------------------------Kruskal test-------------------------
+  if(length(unique(qcdata$condition_col)) > 2){
+    res_pv_kruskal <- rRMSAnalyzer::res_pv(ribo = ribo, test = "kruskal", condition_col = condition_col)
+    k <-rRMSAnalyzer::plot_stat(ribo = ribo, res_pv = res_pv_kruskal, pthr = pthr, condition_col = condition_col, cscore_cutoff = cscore_cutoff)
+  }
+  
+  #----------------------------------------Wilcoxon test------------------------
+  x_list <- list()
+  for(i in seq_len(nrow(comparisons))) {
+    ctrl <- comparisons$ctrl[i]
+    cases <- comparisons$cases[i]
+    
+    ribo_filtered_x <- ribo
+    kept_samples_x <- ribo$metadata %>%
+      dplyr::filter(!!sym(condition_col) %in% c(cases, ctrl))%>%
+      dplyr::pull(samplename)
+    
+    ribo_filtered_x <- keep_ribo_samples(ribo_filtered_x,kept_samples_x)
+    
+    res_pv_wolcoxon <- rRMSAnalyzer::res_pv(ribo = ribo_filtered_x, test = "wilcoxon", condition_col = condition_col)
+    
+    x <- rRMSAnalyzer::plot_stat(ribo = ribo_filtered_x, res_pv = res_pv_wolcoxon, pthr = pthr, condition_col = condition_col, cscore_cutoff = cscore_cutoff)
+    x_list[[i]] <- x 
+  }
+  
+  #-------------------------------------Summary table creation------------------
+  # Init list
+  comparison_results <- list()
+  
+  # Fill each comparison
+  for (i in seq_len(nrow(comparisons))) {
+    ctrl <- comparisons$ctrl[i]
+    case <- comparisons$cases[i]
+    comp_id <- paste0(case, "_vs_", ctrl)
+    
+    # Extract significant sites from tests
+    sites_welch <- w_list[[i]][["plot_env"]][["significant_sites"]]
+    sites_wilcox <- x_list[[i]][["plot_env"]][["significant_sites"]]
+    
+    # Add to the list
+    comparison_results[[comp_id]] <- list(
+      Welch = sites_welch,
+      Wilcoxon = sites_wilcox
+    )
+  }
+  
+  # Init list
+  results <- list()
+  
+  # Identify all significant sites in at least on test
+  if(length(unique(qcdata$condition_col)) > 2){
+    sites_anova <- a[["plot_env"]][["significant_sites"]]
+    sites_kw <- k[["plot_env"]][["significant_sites"]]
+  }
+  sites_welch <- w_list[[i]][["plot_env"]][["significant_sites"]]
+  sites_wilcox <- x_list[[i]][["plot_env"]][["significant_sites"]]
+  
+  # all sites
+  if(length(unique(qcdata$condition_col)) > 2){
+  all_sites <- unique(c(sites_anova, sites_kw, sites_welch, sites_wilcox))
+  } else {
+    all_sites <- unique(c(sites_welch, sites_wilcox))
+  }
+  
+  # first column
+  results$Site <- all_sites
+  
+  # Add Anova et Kruskal column if groups > 2 
+  if (length(unique(qcdata$condition_col)) > 2) {
+    results$Multiple_comparison_Anova <- ifelse(all_sites %in% sites_anova, "Significant", "NS")
+    results$Multiple_comparison_Kruskal_Wallis <- ifelse(all_sites %in% sites_kw, "Significant", "NS")
+    }
+  
+  # Create rest of column dynamically with comparison table
+  for (i in seq_len(nrow(comparisons))) {
+    ctrl <- comparisons$ctrl[i]
+    case <- comparisons$cases[i]
+    
+    comp_id <- paste0(case, "_vs_", ctrl)
+    
+    # Store significants site per test
+    sites_welch_i <- comparison_results[[comp_id]][["Welch"]]
+    sites_wilcox_i <- comparison_results[[comp_id]][["Wilcoxon"]]
+    
+    # Add Welch column 
+    colname_welch <- paste0(comp_id, "_Welch")
+    results[[colname_welch]] <- ifelse(all_sites %in% sites_welch_i, "Significant", "NS")
+    
+    # Add Wilcoxon column
+    colname_wilcox <- paste0(comp_id, "_Wilcoxon")
+    results[[colname_wilcox]] <- ifelse(all_sites %in% sites_wilcox_i, "Significant", "NS")
+  }
+  
+  # Transform in data frame
+  summary_table <- as.data.frame(results)
+}
---
+++ R/render_html_wilcoxon.R
@@ -0,0 +1,97 @@
+#' Print Wilcoxon result in report
+#'
+#' @param ribo a RiboClass object
+#' @param condition_col the condition column used for calculation in metadata
+#' @param ctrl control in comparisons table
+#' @param cases case in comparison table
+#' @param pthr threshold of the p_value
+#' @param cscore_cutoff threshold of c-score
+#'
+#' @returns a tag list
+#' @export
+#' @keywords internal
+#' @examples 
+#' data("ribo_toy")
+#' data("human_methylated")
+#' ribo_toy <- remove_ribo_samples(ribo_toy,c("RNA1", "RNA2"))
+#' ribo_toy <- rename_rna(ribo_toy)  
+#' ribo_toy <- annotate_site(ribo_toy,
+#'                                 annot = human_methylated,
+#'                                 anno_rna = "rRNA",
+#'                                 anno_pos = "Position",
+#'                                 anno_value = "Nomenclature")
+#' comparisons <- tibble::tibble(comp = c("comp1"),
+#'                               ctrl = c("cond1"),
+#'                               cases = c("cond2") )  
+#' ctrl <- comparisons$ctrl
+#' cases <- comparisons$cases 
+#' render_html_wilcoxon(ribo_toy, "condition", ctrl, cases, pthr = 0.05, cscore_cutoff = 0.05)
+render_html_wilcoxon <- function(ribo, condition_col, ctrl, cases, pthr, cscore_cutoff) {
+  ribo_filtered <- ribo
+  kept_samples <- ribo_filtered$metadata %>%
+    dplyr::filter(!!rlang::sym(condition_col) %in% c(cases, ctrl)) %>%
+    dplyr::pull(samplename)
+  
+  ribo_filtered <- keep_ribo_samples(ribo_filtered, kept_samples)
+  
+  res_pv <- rRMSAnalyzer::res_pv(ribo = ribo_filtered, test = "wilcoxon", condition_col = condition_col)
+  
+  res_pv_print <- res_pv
+  if ("p_value" %in% names(res_pv_print)) {
+    res_pv_print$p_value <- formatC(res_pv_print$p_value, format = "e", digits = 3)
+  }
+  if ("p_adj" %in% names(res_pv_print)) {
+    res_pv_print$p_adj <- formatC(res_pv_print$p_adj, format = "e", digits = 3)
+  }
+  if ("delta_c_score" %in% names(res_pv_print)) {
+    res_pv_print$delta_c_score <- round(res_pv_print$delta_c_score, 3)
+  }
+  
+  # Creation of the table
+  dt <- DT::datatable(res_pv_print,
+                  rownames = FALSE,
+                  extensions = c("Buttons"),
+                  options = list(
+                    pageLength = 20,
+                    scrollX = TRUE,
+                    dom = 'Bfrtip',
+                    buttons = list(
+                      list(extend = 'csv', filename = 'summary_table_of_Welch_test'),
+                      list(extend = 'excel', filename = 'summary_table_of_Welch_test'),
+                      list(extend = 'pdf', filename = 'summary_table_of_Welch_test')
+                    )
+                  ))
+  
+  x <- rRMSAnalyzer::plot_stat(ribo = ribo_filtered, site = NULL, res_pv = res_pv, pthr = pthr, condition_col = condition_col, cscore_cutoff = cscore_cutoff)
+  empty_x <- list(plot_env = list(significant_sites = character(0)))
+  # Conditionnal text
+  text <- if (length(x[["plot_env"]][["significant_sites"]]) != 0) {
+    glue::glue("This panel displays boxplots of C-scores for rRNA 2'O-methylation 
+    (2'Ome) sites identified as significantly different between the two biological 
+    conditions {cases} and {ctrl}, based on Wilcoxon rank-sum test results. 
+    Significance was defined by the criteria: adjusted p-value < 0.05 and 
+    |deltaC-score| > 0.05, where deltaC-score = Median C-score_{cases} - Median C-score_{ctrl}.")
+  } else {
+    glue::glue("No significantly differentially methylated rRNA 2'O-methylation (2'Ome) 
+         sites were identified under the current threshold (Wilcoxon rank-sum test,
+         adjusted p-value < 0.05 &  |deltaC-score| > 0.05).")
+  }
+  
+  # Return all in tagList 
+  output_list <- list(
+    htmltools::tags$h4(glue::glue("{cases} vs {ctrl}")),
+    dt,
+    htmltools::tags$p(htmltools::HTML(text))
+  )
+  
+  if (length(x[["plot_env"]][["significant_sites"]]) != 0) {
+    # return plot in tagList and return NULL
+    return(list(
+      ui = htmltools::tagList(output_list),
+      plot = x
+    ))
+  } else {
+    return(list(
+      ui = htmltools::tagList(output_list), plot = empty_x))
+  }
+}
